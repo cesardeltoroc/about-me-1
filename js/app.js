0d1bb47e0ce90d04faf44c1f10132b6842385b9e
Cesar Deltoro <cesardeltoro@Cesars-MacBook-Air.local>
2021-12-02 19:15:57 -0600

    Created a function for guesing game with one correct answer

--- js/app.js
+++ js/app.js
@@ -81,32 +81,36 @@ questionFive();
 
 
 
-// //Question 6
-// let userAnswer = parseInt(prompt('Please guess a Number 1-100'));
-// let correctAnswer = 90
-// let attemps = 4
+//Question 6
+function guessNumber () {
+  let userAnswer = parseInt(prompt('Please guess a Number 1-100'));
+  let correctAnswer = 90
+  let attemps = 4
+  
+  for (let i = 0; i < attemps; i++) {
+    while (userAnswer < 1 || userAnswer > 100) {
+      userAnswer = parseInt(prompt('Please Try Again! A number 1-100'));
+    }
+    if (userAnswer === correctAnswer) {
+      alert('DING DING DING.You got it right');
+      alert('Game Winner!!');
+      counter++;
+      break;
+    } else if (userAnswer > correctAnswer) {
+      userAnswer = parseInt(prompt('Sorry. Too high.Please try again a Number 1-100 '))
+    } else if (userAnswer < correctAnswer) {
+      userAnswer = parseInt(prompt('Sorry.Too low.Please try again a Number 1-100'))
+    }
+  
+    if (i == 3) {
+      alert(`the correct answer was  ${correctAnswer} Game Over!`);
+  
+    }
+  
+  }
+}
+guessNumber();
 
-// for (let i = 0; i < attemps; i++) {
-//   while (userAnswer < 1 || userAnswer > 100) {
-//     userAnswer = parseInt(prompt('Please Try Again! A number 1-100'));
-//   }
-//   if (userAnswer === correctAnswer) {
-//     alert('DING DING DING.You got it right');
-//     alert('Game Winner!!');
-//     counter++;
-//     break;
-//   } else if (userAnswer > correctAnswer) {
-//     userAnswer = parseInt(prompt('Sorry. Too high.Please try again a Number 1-100 '))
-//   } else if (userAnswer < correctAnswer) {
-//     userAnswer = parseInt(prompt('Sorry.Too low.Please try again a Number 1-100'))
-//   }
-
-//   if (i == 3) {
-//     alert(`the correct answer was  ${correctAnswer} Game Over!`);
-
-//   }
-
-// }
 
 
 // //Question 7
