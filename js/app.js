6286e46e8fe36eb690abae34501995b971a541a8
Hambalieu Jallow <hambalieujallow@gmail.com>
2021-12-02 17:01:55 -0800

    Added a guessing game with multiple correct answers

--- js/app.js
+++ js/app.js
@@ -1,111 +1,118 @@
 'use strict';
 //console.log ('hello world');//
-let userName = prompt ('What is your name?')
-    alert('Welcome to Jallow\'s About Me website ' + userName);
+let counter = 0;
+
+let userName = prompt('What is your name?')
+alert('Welcome to Jallow\'s About Me website ' + userName);
 
 
-//Question one//
+//Question 1
 let userAgeOne = prompt('Am I 29 years old?').toLowerCase();
 
-if (userAgeOne === 'yes' || userAgeOne === 'y'){
+if (userAgeOne === 'yes' || userAgeOne === 'y') {
   //console.log('YES, I am 29 years old');
   alert('Yes I am 29 years old. ');
-  
+  counter++;
+
 }
 
-
+//Question 2
 let userLocation = prompt('Do I live in Seattle?').toLowerCase();
 
-if (userLocation === 'yes' || userLocation === 'y'){
+if (userLocation === 'yes' || userLocation === 'y') {
   //console.log('Yes I do live live in Seattle');
   alert('Yes I do live in Seattle. ');
+  counter++;
 
-} 
+}
 
-
+//Question 3
 let userVideoGame = prompt('Do I play video games?').toLowerCase();
 
-if (userVideoGame === 'yes' || userVideoGame === 'y'){
+if (userVideoGame === 'yes' || userVideoGame === 'y') {
   //console.log('Yes I do play video games.');
   alert('Yes I do play video games. ');
+  counter++;
 }
 
+
+//Question 4
 let userSoccer = prompt('Do I like to play soccer?').toLowerCase();
 
-if (userSoccer === 'yes' || userSoccer === 'y'){
+if (userSoccer === 'yes' || userSoccer === 'y') {
   //console.log('Yes I do like to play soccer.');
   alert('Yes I do like to play soccer.');
+  counter++;
 }
 
 
+
+//Question 5
 let userCake = prompt('Do I like cake?').toLowerCase();
 
-if (userCake === 'yes' || userCake === 'y'){
+if (userCake === 'yes' || userCake === 'y') {
   //console.log('Yes I do like cake.');
   alert('Yes I do like cake. ');
+  counter++;
 }
 
 
 
+//Question 6
+let userAnswer = parseInt(prompt('Please guess a Number 1-100'));
+let correctAnswer = 90
+let attemps = 4
 
-  let userAnswer = parseInt(prompt('Please guess a Number 1-100'));
-  let correctAnswer = 90
-  let attemps = 3
-  
-  for (let i=0; i < attemps; i++){
-      while(userAnswer < 1 || userAnswer > 100){
-          userAnswer = parseInt(prompt ('Please Try Again! A number 1-100'))
-      }
-      if (userAnswer === correctAnswer){
-          alert('DING DING DING.You got it right');
-          alert('Game Winner!!');
-          break;
-      }else if (userAnswer > correctAnswer){
-          userAnswer = parseInt(prompt('Sorry. Too high.Please try again a Number 1-100 ') )  
-      }else if (userAnswer < correctAnswer){
-          userAnswer = parseInt(prompt('Sorry.Too low.Please try again a Number 1-100'))
-      }
+for (let i = 0; i < attemps; i++) {
+  while (userAnswer < 1 || userAnswer > 100) {
+    userAnswer = parseInt(prompt('Please Try Again! A number 1-100'));
+  }
+  if (userAnswer === correctAnswer) {
+    alert('DING DING DING.You got it right');
+    alert('Game Winner!!');
+    counter++;
+    break;
+  } else if (userAnswer > correctAnswer) {
+    userAnswer = parseInt(prompt('Sorry. Too high.Please try again a Number 1-100 '))
+  } else if (userAnswer < correctAnswer) {
+    userAnswer = parseInt(prompt('Sorry.Too low.Please try again a Number 1-100'))
+  }
 
-      if (i == 3){
-          alert('the correct answer was '  + correctAnswer);
-          alert('Game Loser!!');
-
-      }
+  if (i == 3) {
+    alert(`the correct answer was  ${correctAnswer} Game Over!`);
 
   }
 
+}
+
+
+//Question 7
+let userQ7Guess =parseInt(prompt('What are the Factors of 12?'));
+let correctAnswersArray = [1, 2, 3, 4, 6, 12];
+
+
+for (let count=5; count > 0; count--){
+  for (let i = 0;i < correctAnswersArray.length; i++){
+    let correctNum = correctAnswersArray[i];
+    if (userQ7Guess === correctNum){
+      alert ('You got it Correct!');
+      count = 1;
+      break;
+    }
+    else if (userQ7Guess !== correctNum){
+      userQ7Guess = prompt('Thats not right try again!');
+    }
+  if (i == 4){
+    alert(`You are out of tries the correct answer was ${correctAnswersArray} `);
+    count = 1
+    break;
+    }
+
+  }
+  
+  
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  alert('Thank!! you for visiting my website and participate in the games  '  + userName);
+alert(`Thank!! you for visiting my website and participate in the games ${userName} ..You got ${counter}/6 .`);
